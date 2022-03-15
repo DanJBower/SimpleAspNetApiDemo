bc10e6455fa9e2711cd971b5cbd6db51a7738931
DanJBower <bowerdanielj@gmail.com>
2022-03-15 01:31:26 +0000

    Ugh removing interfaces not needed for now

---
+++ SimpleAspNetApiDemo/SimpleAspNetApiDemo/DataAccess/SchoolDatabase.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Data.Sqlite;
+using System.Data.Common;
+
+namespace SimpleAspNetApiDemo.DataAccess
+{
+    public static class SchoolDatabase
+    {
+        public static DbConnection GetSchoolDbConnection()
+        {
+            return new SqliteConnection("Data Source=data.db;");
+        }
+    }
+}
--- SimpleAspNetApiDemo/SimpleAspNetApiDemo/Program.cs
+++ SimpleAspNetApiDemo/SimpleAspNetApiDemo/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using SimpleAspNetApiDemo.DataAccess;
+using System.Data.Common;
 
 namespace SimpleAspNetApiDemo
 {
@@ -9,6 +11,11 @@ namespace SimpleAspNetApiDemo
         {
             // Ensure Database exists
             // If doesn't exist, add base data
+            using (DbConnection connection = SchoolDatabase.GetSchoolDbConnection())
+            {
+                connection.Open();
+                connection.BuildSample();
+            }
 
             // Run API
             CreateHostBuilder(args).Build().Run();
